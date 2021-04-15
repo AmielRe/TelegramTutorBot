c5ae803e4cba479af87370418f4153c8a9f35356
AmielRe <amieli1999@gmail.com>
2021-04-15 22:56:44 +0300

    Add logs
    Add logs for exceptions.
    Check if any dates exists and if not, display appropriate message to user.

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def sendMessage(chat_id,text_message):
     return response
 
 def sendInlineMessageForService(chat_id):
-    text_message='שלום! אני הבוט שלך עבור קביעת שיעורים פרטיים עם עדן!\n\n:אתה יכול לשלוט בי באמצעות הפקודות\n\n\"שלום\" - להתחיל לדבר עם הבוט\n\"די\" - להפסיק לדבר עם הבוט.\n'
+    text_message='שלום! אני הבוט שלך עבור קביעת שיעורים פרטיים עם עדן!\n\nאתה יכול לשלוט בי באמצעות הפקודות:\n\n\"שלום\" - להתחיל לדבר עם הבוט\n\"די\" - להפסיק לדבר עם הבוט.\n'
     keyboard={'keyboard':[
                         [{'text':'שנה שיעור'},{'text':'בטל שיעור'}],
                         [{'text':'שיעור חדש'}]]}
@@ -79,62 +79,77 @@ def sendInlineMessageForBookingTime(chat_id):
     text_message='בחר זמן עבור השיעור...'
     dates = get_SplitedFreeSlots()
         
-    keyboard={'keyboard': dates}
-    key=json.JSONEncoder().encode(keyboard)
-    url='https://api.telegram.org/bot'+str(api_key)+'/sendmessage?chat_id='+str(chat_id)+'&text='+str(text_message)+'&reply_markup='+key
-    response = requests.get(url)
-    return response
-
+    if(len(dates) <= 0):
+        sendMessage(chat_id, "לצערנו אין חלון זמן פנוי כרגע" + "\n" + "אנא נסה שנית במועד מאוחר יותר" + "\n" + "תודה!")
+    else:
+        keyboard={'keyboard': dates}
+        key=json.JSONEncoder().encode(keyboard)
+        url='https://api.telegram.org/bot'+str(api_key)+'/sendmessage?chat_id='+str(chat_id)+'&text='+str(text_message)+'&reply_markup='+key
+        response = requests.get(url)
+        return response
 
 def run():
+    logf = open("C:\\Users\\Public\\BotLog.txt", "w")
     update_id_for_booking_of_time_slot=''
     prev_last_msg,chat_id,prev_update_id,user_name=getLastMessage()
     while True:
-        current_last_msg,chat_id,current_update_id,user_name=getLastMessage()
-        if prev_last_msg==current_last_msg and current_update_id==prev_update_id:
-            print('Listen')
-            events = get_availableSlots()
-            print(len(events))
-            continue
-        else:
-            if current_last_msg=='/start' or current_last_msg=='start' or current_last_msg=='היי' or current_last_msg=='שלום':
-                sendInlineMessageForService(chat_id)   
-            if current_last_msg in ['בטל שיעור','שנה שיעור','שיעור חדש']:
-                sendInlineMessageForBookingTime(chat_id)
-            if validateDate(current_last_msg):
-                booking_time=current_last_msg
-                update_id_for_booking_of_time_slot=current_update_id
-                sendMessage(chat_id,"הכנס כתובת מייל:")
-            if current_last_msg=='די':
-                update_id_for_booking_of_time_slot=''
-                # return
+        try:
+            current_last_msg,chat_id,current_update_id,user_name=getLastMessage()
+            if prev_last_msg==current_last_msg and current_update_id==prev_update_id:
+                events = get_availableSlots()
                 continue
-            if update_id_for_booking_of_time_slot!=current_update_id and update_id_for_booking_of_time_slot!= '':
-                if check_email(current_last_msg)==True:
+            else:
+                if current_last_msg=='/start' or current_last_msg=='start' or current_last_msg=='היי' or current_last_msg=='שלום':
+                    sendInlineMessageForService(chat_id)  
+                    logf = open("C:\\Users\\Public\\BotLog.txt", "w")
+                    logf.write("hey")
+                    logf.close()
+                if current_last_msg in ['בטל שיעור','שנה שיעור','שיעור חדש']:
+                    logf = open("C:\\Users\\Public\\BotLog.txt", "w")
+                    logf.write("1")
+                    logf.close()
+                    sendInlineMessageForBookingTime(chat_id)
+                    logf = open("C:\\Users\\Public\\BotLog.txt", "w")
+                    logf.write("2")
+                    logf.close()
+                if validateDate(current_last_msg):
+                    booking_time=current_last_msg
+                    update_id_for_booking_of_time_slot=current_update_id
+                    sendMessage(chat_id,"הכנס כתובת מייל:")
+                if current_last_msg=='די':
                     update_id_for_booking_of_time_slot=''
-                    sendMessage(chat_id,"קובע, אנא המתן.....")
-                    input_email=current_last_msg
-                    dates = get_SplitedFreeSlotsWithEvent()
-                    dates = [currDate for currDate in dates if currDate[0] != booking_time]
-                    delete_availableSlot(booking_time)
-                    for currDate in dates:
-                        placeFreeTimeSlot(currDate[0])
-                    response=book_timeslot(booking_time,input_email,user_name)
-                    if response == True:
-                        sendMessage(chat_id,"שיעור נקבע! נתראה ב - " + str(booking_time))
-                        sendMessage("860442422", "שיעור חדש נקבע עם " + str(user_name) + " ב - " + str(booking_time))
-                        continue
-                    else:
+                    # return
+                    continue
+                if update_id_for_booking_of_time_slot!=current_update_id and update_id_for_booking_of_time_slot!= '':
+                    if check_email(current_last_msg)==True:
                         update_id_for_booking_of_time_slot=''
-                        sendMessage(chat_id,"בבקשה נסה חלון זמן אחר ונסה שוב מחר")
-                        continue
-                else:
-                    sendMessage(chat_id,"יש להכניס מייל תקין.\nהקש \"די\" על מנת להפסיק לדבר עם הבוט\nתודה!")
-
-                        
-        prev_last_msg=current_last_msg
-        prev_update_id=current_update_id
-        
+                        sendMessage(chat_id,"קובע, אנא המתן.....")
+                        input_email=current_last_msg
+                        dates = get_SplitedFreeSlotsWithEvent()
+                        dates = [currDate for currDate in dates if currDate[0] != booking_time]
+                        delete_availableSlot(booking_time)
+                        for currDate in dates:
+                            placeFreeTimeSlot(currDate[0])
+                        response=book_timeslot(booking_time,input_email,user_name)
+                        if response == True:
+                            sendMessage(chat_id,"שיעור נקבע! נתראה ב - " + str(booking_time))
+                            #sendMessage("860442422", "שיעור חדש נקבע עם " + str(user_name) + " ב - " + str(booking_time))
+                            continue
+                        else:
+                            update_id_for_booking_of_time_slot=''
+                            sendMessage(chat_id,"בבקשה נסה חלון זמן אחר ונסה שוב מחר")
+                            continue
+                    else:
+                        sendMessage(chat_id,"יש להכניס מייל תקין.\nהקש \"די\" על מנת להפסיק לדבר עם הבוט\nתודה!")
+    
+                            
+            prev_last_msg=current_last_msg
+            prev_update_id=current_update_id
+        except Exception as e:
+            logf = open("C:\\Users\\Public\\BotLog.txt", "w")
+            logf.write("Fail: {0}\n".format(str(e)))
+            logf.close()
+            continue
             
 if __name__ == "__main__":
     run()
